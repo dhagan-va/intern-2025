b56a4a56a159c2d0d8d1f7543724d897997774bc
ryanl <ryan.y.lee0509@gmail.com>
2025-06-05 16:41:46 -0400

    defined method for PolicyID2 and FID

--- test-tools/DataGen/834filecreation.py
+++ test-tools/DataGen/834filecreation.py
@@ -39,7 +39,6 @@ if not os.path.exists(file_directory):
 edi_name = f"834.VFMP.{now.year}.{yymmdd}.{hhmm}.{ISA13}.edi"
 edi_path = os.path.join(file_directory, edi_name)
 
-# Write to file
 f = open(edi_path, "w")
 
 ISA = (f"ISA*00*          *00*          *{ISA05_07}*{ISA06:<15}*{ISA05_07}*{ISA08:<15}*{yymmdd}*{hhmm}*$*00501"
@@ -49,11 +48,19 @@ f.write(ISA)
 f.write(GS)
 
 # number of tests
-n = 100_000
+n = 10
 
 
 def getIns02():
     return random.choices(INS02_VALUES, weights=weights, k=1)[0]
+
+
+def getFid():
+    return random.randint(100_000_000, 999_999_999)
+
+
+def getPolicyID2():
+    return random.randint(10_000_000, 99_999_999)
 
 
 for interval in range(1, n + 1):
@@ -65,18 +72,15 @@ for interval in range(1, n + 1):
     ins02 = getIns02()
     counts[ins02] += 1
 
-    # Lookup uses ID instead of SSN
-
     # INS02 null, 01, 18, 19, 25, 26, G8
-    # INS03 001 (change) or 030 (audit)
     # INS08 always null, AC, TE
     segments = [f"ST*834*{interval:04}~\n",
                 f"BGN*00*{uuid.uuid4().hex.upper()}*{ccyymmdd}*{hhmmss}*UT***2~\n",
-                f"N1*P5*{re.sub('[^A-Za-z]+', ' ', fake.company())}*FI*{random.randint(100_000_000, 999_999_999)}~\n",
-                f"N1*IN*{re.sub('[^A-Za-z]+', ' ', fake.company())}*FI*{random.randint(100_000_000, 999_999_999)}~\n",
+                f"N1*P5*{fake.company()}*FI*{getFid()}~\n",
+                f"N1*IN*{fake.company()}*FI*{getFid()}~\n",
                 f"INS*Y*{getIns02()}*001**A***AC~\n",
-                f"REF*0F*1111111111V{random.randint(10_000_000, 99_999_999)}~\n",
-                f"REF*6O*1111111111V{random.randint(10_000_000, 99_999_999)}~\n",
+                f"REF*0F*1111111111V{getPolicyID2()}~\n",
+                f"REF*6O*1111111111V{getPolicyID2()}~\n",
                 f"NM1*IL*1*{fake.last_name().upper()}*{fake.first_name().upper()}*{fake.first_name().upper()}***34*{ssn}~\n",
                 f"PER*IP**TE*{re.sub('[^0-9]+', '', fake.basic_phone_number())}~\n",
                 f"N3*{fake.building_number()} {fake.street_name()}*{n302}~\n",
@@ -105,12 +109,15 @@ IEA = f"IEA*1*{ISA13}~"
 f.write(GE)
 f.write(IEA)
 
+# Lookup sponsor ID instead of SSN
 # keep track of sponsor ID, first, middle, last name, address, SSN, beneficiary ID, phone
 
 f.close()
+# display amount of time it takes to create
 END_TIME = datetime.now() - now
 print("It took: ", end='')
 print(END_TIME)
+
 print("The distribution of INS02 is as follows: ")
 for k, v in counts.items():
     print(f"{k}: {v}")

53dde0ce76cb2b829cb0d5bf7f9bd9c8db03eb6f
ryanl <ryan.y.lee0509@gmail.com>
2025-06-06 16:48:59 -0400

    added normal dist to number of tests, still need to adjust values

--- test-tools/DataGen/config.py
+++ test-tools/DataGen/config.py
@@ -1,10 +1,10 @@
 import logging
-import random
+from BellShapes import BellShapes, fit_range_to_half_bel
 
 FAKER_SEED = 49245
 RANDOM_SEED = 52
 
-NUMBER_OF_TESTS = random.gauss(10_627, 13_948)  # Binom dist for number of tests so that it generates the typical production rate, need to make it redo if it is negative
+NUMBER_OF_TESTS = fit_range_to_half_bel(avg=10627, std=13948, min_val=1, max_val=246778, shape=BellShapes.NORMAL)
 DIRECTORY_NAME = 'Test_Files_834'
 USER_LIMIT = 500_000
 

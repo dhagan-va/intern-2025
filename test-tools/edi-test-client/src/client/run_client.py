abbad607e6363cb224e8da5ef5e1644daf2bb542
chrissu777 <christophersu777@gmail.com>
2025-06-06 11:36:36 -0400

    restructured directories

---
+++ test-tools/edi-test-client/src/client/run_client.py
@@ -0,0 +1,35 @@
+import requests
+from src.config import load_settings
+import time
+
+cfg = load_settings()
+
+s = requests.Session()
+
+count = 0
+
+SAMPLE_270 = (
+"ISA*00*          *00*          *ZZ*1234567        *ZZ*11111          *170508*1141*^*00501*000000101*1*P*:~\n"
+"GS*HC*XXXXXXX*XXXXX*20170617*1741*101*X*005010X279A1~\n"
+"ST*270*1234*005010X279A1~\n"
+"BHT*0022*13*10001234*20060501*1319~\n"
+"HL*1**20*1~\n"
+"NM1*PR*2*ABC COMPANY*****PI*842610001~\n"
+"HL*2*1*21*1~\n"
+"NM1*1P*2*BONE AND JOINT CLINIC*****SV*2000035~\n"
+"HL*3*2*22*0~\n"
+"TRN*1*93175-012547*9877281234~\n"
+"NM1*IL*1*SMITH*ROBERT****MI*11122333301~\n"
+"DMG*D8*19430519~\n"
+"DTP*291*D8*20060501~\n"
+"EQ*30~\n"
+"SE*13*1234~\n"
+"GE*1*101~\n"
+"IEA*1*000000101~\n"
+)
+
+while True:
+    start = time.perf_counter()
+    resp = s.post(url=cfg.endpoint, data=SAMPLE_270)
+    elapsed = (time.perf_counter() - start)*1000
+    time.sleep(max(0, (1 / cfg.rps) - elapsed))
---
+++ test-tools/edi-test-client/src/config.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+import tomllib
+from pathlib import Path
+
+DEFAULT_CONF = Path('conf/default.toml')
+
+@dataclass
+class Setting:
+    """
+    Class respresenting configuration settings for test client.
+    """
+    endpoint: str
+    rps: int
+
+    def __init__(self, endpoint : str, rps : int):
+        self.endpoint = endpoint
+        self.rps = rps
+
+def load_settings():
+    with open(DEFAULT_CONF, 'rb') as f:
+        data = tomllib.load(f)
+        return Setting(data['endpoint'], data['rps'])
+
+

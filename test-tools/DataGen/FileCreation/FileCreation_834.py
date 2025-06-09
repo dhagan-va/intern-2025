46959c6d913759364b6d688e00ac2dc6c4091d1f
ryanl <ryan.y.lee0509@gmail.com>
2025-06-09 09:19:16 -0400

    journal and refactored code

--- test-tools/DataGen/FileCreation/FileCreation_834.py
+++ test-tools/DataGen/FileCreation/FileCreation_834.py
@@ -19,15 +19,15 @@ class Make834:
         self.counts = Counter()
         self.n = n
 
-        self.ISA05_07 = "ZZ"
-        self.ISA06 = "83-1002022"
-        self.ISA08 = "841439824"
-        self.AMT_CODES = ['D2', 'FK', 'R', 'C1', 'P3', 'B9']
+        self.interchangeIDQual = "ZZ"
+        self.interchangeSenderID = "83-1002022"
+        self.interchangeReceiverID = "841439824"
+        self.amtQualCode = ['D2', 'FK', 'R', 'C1', 'P3', 'B9']
         self.INS02_VAL = ['18', '19', '25', '26', '01', 'G8', 'null']
         self.weights = [0.8] + [0.2 / (len(self.INS02_VAL) - 1)] * (len(self.INS02_VAL) - 1)
 
         now = self.time
-        self.ISA13 = now.strftime("%Y%m%d1")
+        self.interCtrlNumber = now.strftime("%Y%m%d1")
         self.ccyymmdd = now.strftime("%Y%m%d")
         self.yymmdd = now.strftime("%y%m%d")
         self.hhmm = now.strftime("%H%M")
@@ -41,23 +41,23 @@ class Make834:
             logger.debug(f"New file directory created called: {file_directory}")
 
     def make_file_path(self, file_directory):
-        edi_name = f"834.VFMP.{self.time.year}.{self.yymmdd}.{self.hhmm}.{self.ISA13}.edi"
+        edi_name = f"834.VFMP.{self.time.year}.{self.yymmdd}.{self.hhmm}.{self.interCtrlNumber}.edi"
         logger.debug(f"New file created named: {edi_name} at {file_directory}")
         return os.path.join(file_directory, edi_name)
 
     def makeISA(self):
         return (
-            f"ISA*00*          *00*          *{self.ISA05_07}*{self.ISA06:<15}*{self.ISA05_07}*{self.ISA08:<15}*{self.yymmdd}*{self.hhmm}*$*00501"
-            f"*{self.ISA13}*0*T*:~\n")
+            f"ISA*00*          *00*          *{self.interchangeIDQual}*{self.interchangeSenderID:<15}*{self.interchangeIDQual}*{self.interchangeReceiverID:<15}*{self.yymmdd}*{self.hhmm}*$*00501"
+            f"*{self.interCtrlNumber}*0*T*:~\n")
 
     def makeGS(self):
-        return f"GS*BE*{self.ISA06}*{self.ISA08}*{self.ccyymmdd}*{self.hhmmss}*61*X*005010X220A1~\n"
+        return f"GS*BE*{self.interchangeSenderID}*{self.interchangeReceiverID}*{self.ccyymmdd}*{self.hhmmss}*61*X*005010X220A1~\n"
 
     def makeGE(self, num):
         return f"GE*{num}*61~\n"
 
     def makeIEA(self):
-        return f"IEA*1*{self.ISA13}~"
+        return f"IEA*1*{self.interCtrlNumber}~"
 
     def getIns02(self):
         return random.choices(self.INS02_VAL, weights=self.weights, k=1)[0]
@@ -76,8 +76,6 @@ class Make834:
         ins02 = self.getIns02()
         self.counts[ins02] += 1
 
-        # INS02 null, 01, 18, 19, 25, 26, G8
-        # INS08 always null, AC, TE
         segments = [f"ST*834*{num:04}~\n",
                     f"BGN*00*{uuid.uuid4().hex.upper()}*{self.ccyymmdd}*{self.hhmmss}*UT***2~\n",
                     f"N1*P5*{self.fake.company()}*FI*{self.getFid()}~\n",
@@ -87,13 +85,15 @@ class Make834:
                     f"REF*6O*1111111111V{self.getPolicyID2()}~\n",
                     f"NM1*IL*1*{self.fake.last_name().upper()}*{self.fake.first_name().upper()}*{self.fake.first_name().upper()}***34*{self.fake.ssn()}~\n",
                     f"PER*IP**TE*{self.fake.basic_phone_number()}~\n",
-                    f"N3*{self.fake.building_number()} {self.fake.street_name()}*{n302}~\n",
+                    f"N3*{self.fake.building_number()}{" "}{self.fake.street_name()}*{n302}~\n",
                     f"N4*{self.fake.city().upper()}*{self.fake.state_abbr().upper()}*{self.fake.zipcode()}~\n"
                     ]
         # add AMT values
-        for code in self.AMT_CODES:
-            # if AMT = D2, FK, R ($)
-            value = random.randint(0, 9)
+        for code in self.amtQualCode:
+            if code == "D2" or code == "FK" or code == "R":
+                value = random.randint(0, 100_000)
+            else:
+                value = random.randint(0, 9)
             if value == 0:
                 continue
             amt_segments.append(f"AMT*{code}*{value}~\n")
@@ -104,6 +104,3 @@ class Make834:
                      f"SE*{segment_count + 3}*{num:04}~\n"
                      ]
         return segments
-
-        # Lookup sponsor ID instead of SSN
-        # keep track of sponsor ID, first, middle, last name, address, SSN, beneficiary ID, phone [hashtable/dict]

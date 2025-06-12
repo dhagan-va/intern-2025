bcf8f27fdfd46e7daabb13308ef9512747dd988c
chrissu777 <christophersu777@gmail.com>
2025-06-12 15:23:19 -0400

    added live configurable rps

--- test-tools/edi-test-client/src/client/load_client.py
+++ test-tools/edi-test-client/src/client/load_client.py
@@ -14,7 +14,8 @@ import reqs
 class LoadClient:
     def __init__(self, cfg: Setting):
         self.endpoint = cfg.endpoint
-        self.rps = cfg.rps
+        self._rps = cfg.rps
+        self._rps_lock = threading.Lock()
         self.threads = cfg.threads
 
         self._running = False
@@ -22,9 +23,9 @@ class LoadClient:
         self._sched_thread = None
         self._stop_event = threading.Event()
 
-        self._log = logging.getLogger("edi_load_client")
-        logging.basicConfig(filename="test.log", level=logging.INFO)
-        
+        self._log = logging.getLogger('edi_load_client')
+        logging.basicConfig(filename='test.log', level=logging.INFO)
+
     def start(self):
         if self._running:
             return
@@ -35,9 +36,9 @@ class LoadClient:
         self._sched_thread = threading.Thread(target=self._scheduler, daemon=True)
         self._pool = ThreadPoolExecutor(max_workers=self.threads)
         self._sched_thread.start()
-        curr_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        curr_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self._log.info(
-            "Client started at %s → %s, %.1f rps, %d threads.",
+            'Client started at %s → %s, %.1f rps, %d threads.',
             curr_time,
             self.endpoint,
             self.rps,
@@ -49,7 +50,7 @@ class LoadClient:
             return
         self._stop_event.set()
         self._running = False
-        self._log.info("Stopping client")
+        self._log.info('Stopping client')
 
         if self._sched_thread:
             self._sched_thread.join()
@@ -57,13 +58,13 @@ class LoadClient:
         if self._pool:
             self._pool.shutdown(wait=True)
 
-        curr_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self._log.info("Client stopped at %s", curr_time)
+        curr_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self._log.info('Client stopped at %s', curr_time)
 
     def _scheduler(self):
         next_run = time.perf_counter()
-        interval = 1.0 / self.rps
         while not self._stop_event.is_set():
+            interval = 1.0 / self.rps
             fut = self._pool.submit(reqs.send_270_request, self.endpoint)
             fut.add_done_callback(self._handle_response)
             next_run += interval
@@ -74,8 +75,20 @@ class LoadClient:
         try:
             status, elapsed = future.result()
             if status == 200:
-                self._log.info("200 OK in %.3f ms", elapsed)
+                self._log.info('200 OK in %.3f ms', elapsed)
             else:
-                self._log.error("ERR %d in %.3f ms", status, elapsed)
+                self._log.error('ERR %d in %.3f ms', status, elapsed)
         except Exception as e:
-            self._log.error("Error handling response: %s", e)
+            self._log.error('Error handling response: %s', e)
+
+    @property
+    def rps(self) -> float:
+        with self._rps_lock:
+            return self._rps
+
+    def update_rps(self, new_rps: float):
+        with self._rps_lock:
+            self._rps = new_rps
+
+        curr_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self._log.info('RPS updated at %s to %.1f', curr_time, new_rps)

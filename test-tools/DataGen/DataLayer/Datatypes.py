c1931c1f2ee7e713210a1c2676be40faac07a04e
ryanl <ryan.y.lee0509@gmail.com>
2025-06-12 13:53:54 -0400

    localdb functions

--- test-tools/DataGen/DataLayer/Datatypes.py
+++ test-tools/DataGen/DataLayer/Datatypes.py
@@ -1,7 +1,7 @@
 """
 Defines data classes for database
 """
-import datetime
+from datetime import datetime
 from dataclasses import dataclass, field
 from typing import List, Dict, Optional
 
@@ -14,6 +14,16 @@ class Address:
     city: str
     state: str
     zipcode: str
+
+    def to_dict(self):
+        return {
+            "building_number": self.building_number,
+            "street": self.street,
+            "apartment": self.apartment,
+            "city": self.city,
+            "state": self.state,
+            "zipcode": self.zipcode,
+        }
 
 
 @dataclass
@@ -37,6 +47,24 @@ class Beneficiary(Base):
     deductibles: Dict[str, int] = field(default_factory=dict)
     visit_counts: Dict[str, int] = field(default_factory=dict)
 
+    def to_dict(self):
+        return {
+            "ssn": self.ssn,
+            "dob": self.dob,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "address": self.address.to_dict(),
+            "phone": self.phone,
+            "insurance_company": self.insurance_company,
+            "insurance_FID": self.insurance_FID,
+            "sponsor_id": self.sponsor_id,
+            "beneficiary_id": self.beneficiary_id,
+            "relationship": self.relationship,
+            "middle_name": self.middle_name,
+            "deductibles": self.deductibles,
+            "visit_counts": self.visit_counts
+        }
+
 
 @dataclass
 class Sponsor(Base):
@@ -45,3 +73,20 @@ class Sponsor(Base):
     deductibles: Dict[str, float] = field(default_factory=dict)
     visit_counts: Dict[str, int] = field(default_factory=dict)
     beneficiaries: List[Beneficiary] = field(default_factory=list)
+
+    def to_dict(self):
+        return {
+            "ssn": self.ssn,
+            "dob": self.dob.isoformat(),
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "address": self.address.to_dict(),
+            "phone": self.phone,
+            "insurance_company": self.insurance_company,
+            "insurance_FID": self.insurance_FID,
+            "sponsor_id": self.sponsor_id,
+            "middle_name": self.middle_name,
+            "deductibles": self.deductibles,
+            "visit_counts": self.visit_counts,
+            "beneficiaries": [b.to_dict() for b in self.beneficiaries]
+        }
---
+++ test-tools/DataGen/Repository/Local_Database_Functions.py
@@ -0,0 +1,102 @@
+import json
+import logging
+import os
+import config
+from DataLayer.Interfaces import DataAccess
+
+logger = logging.getLogger(__name__)
+
+
+class LocalDBFunctions(DataAccess):
+    def __init__(self, file=config.LOCAL_DATABASE):
+        self.data = None
+        self.file = file
+        self.loadfile()
+
+    def loadfile(self):
+        if not os.path.exists(self.file):
+            with open(self.file, "w") as f:
+                json.dump([], f)
+        with open(self.file, "r") as f:
+            self.data = json.load(f)
+
+    def save_sponsor(self, sponsor):
+        self.data.append(sponsor.to_dict())
+
+    def save_all(self):
+        with open(self.file, "w") as f:
+            json.dump(self.data, f)
+
+    def ssn_exists(self, ssn):
+        for sponsor in self.data:
+            if sponsor["ssn"] == ssn:
+                return True
+            for b in sponsor.get("beneficiaries", []):
+                if b["ssn"] == ssn:
+                    return True
+        return False
+
+    def get_sponsor_by_id(self, sponsor_id):
+        return next((s for s in self.data if s["sponsor_id"] == sponsor_id), None)
+
+    def get_sponsor_field(self, sponsor_id, field):
+        sponsor = self.get_sponsor_by_id(sponsor_id)
+        return sponsor.get(field) if sponsor else None
+
+    def update_sponsor_field(self, sponsor_id, field, value):
+        for sponsor in self.data:
+            if sponsor["sponsor_id"] == sponsor_id:
+                sponsor[field] = value
+                return True
+        return False
+
+    def get_beneficiary(self, sponsor_id, beneficiary_id):
+        sponsor = self.get_sponsor_by_id(sponsor_id)
+        if sponsor:
+            for b in sponsor.get("beneficiaries", []):
+                if b["beneficiary_id"] == beneficiary_id:
+                    return b
+        return None
+
+    def update_beneficiary_field(self, sponsor_id, beneficiary_id, field, value):
+        sponsor = self.get_sponsor_by_id(sponsor_id)
+        if sponsor:
+            for b in sponsor.get("beneficiaries", []):
+                if b["beneficiary_id"] == beneficiary_id:
+                    b[field] = value
+                    return True
+        return False
+
+    def get_beneficiary_field(self, sponsor_id, beneficiary_id, field):
+        beneficiary = self.get_beneficiary(sponsor_id, beneficiary_id)
+        return beneficiary.get(field) if beneficiary else None
+
+    def get_bene_deductible(self, sponsor_id, beneficiary_id, code):
+        beneficiary = self.get_beneficiary(sponsor_id, beneficiary_id)
+        if beneficiary:
+            return beneficiary.get("deductibles", {}).get(code)
+        return None
+
+    def get_bene_visit(self, sponsor_id, beneficiary_id, code):
+        beneficiary = self.get_beneficiary(sponsor_id, beneficiary_id)
+        if beneficiary:
+            return beneficiary.get("visit_counts", {}).get(code)
+        return None
+
+    def update_bene_deductible(self, sponsor_id, beneficiary_id, code, value):
+        sponsor = self.get_sponsor_by_id(sponsor_id)
+        if sponsor:
+            for b in sponsor.get("beneficiaries", []):
+                if b["beneficiary_id"] == beneficiary_id:
+                    b["deductibles"][code] = value
+                    return True
+        return False
+
+    def update_bene_visit(self, sponsor_id, beneficiary_id, code, value):
+        sponsor = self.get_sponsor_by_id(sponsor_id)
+        if sponsor:
+            for b in sponsor.get("beneficiaries", []):
+                if b["beneficiary_id"] == beneficiary_id:
+                    b["visit_counts"][code] = value
+                    return True
+        return False

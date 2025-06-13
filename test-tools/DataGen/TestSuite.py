b849ff3b5391b7f29648d02aa39e8577303ca100
ryanl <ryan.y.lee0509@gmail.com>
2025-06-13 16:15:22 -0400

    logging and filename for output file

--- test-tools/DataGen/TestSuite.py
+++ test-tools/DataGen/TestSuite.py
@@ -10,7 +10,7 @@ from config import get_logger
 # Setup/Initialization
 now = datetime.now()
 directory = config.OUTPUT_DIRECTORY_NAME
-n = 50000
+n = 5
 logger = get_logger(__name__)
 
 # Generate 834 Data
@@ -20,14 +20,16 @@ edi_generator = EDI834Generator()
 # Generate Fake Data
 logger.info(f"Generating {n} families")
 data_creation.create_sponsor_and_beneficiaries(n)
+logger.info(f"Data loading Initiated")
 data_creation.repo.loadfile()
+logger.info(f"Data Loading took: {datetime.now() - now}")
 sponsor_data = data_creation.repo.data
 logger.info(f"Data generation complete")
 logger.info(f"Data generation took: {datetime.now() - now}")
 
 # Generate EDI File
 logger.info("Generating EDI file from stored data")
-edi_file = edi_generator.generate_file(Sponsor.from_dict(s) for s in sponsor_data)
+edi_file = edi_generator.combine_segments(Sponsor.from_dict(s) for s in sponsor_data)
 logger.info("EDI file generation complete")
 logger.info(f"File generation took: {datetime.now() - now}")
 
@@ -36,8 +38,8 @@ if not os.path.exists(config.OUTPUT_DIRECTORY_NAME):
     os.mkdir(config.OUTPUT_DIRECTORY_NAME)
     logger.info(f"Created directory: {config.OUTPUT_DIRECTORY_NAME}")
 
-file_name = f'834.VFMP.{now.year}.{now.strftime("%y%m%d")}.{now.strftime("%H%M")}.{now.strftime("%Y%m%d1")}.edi'
-file_path = os.path.join(config.OUTPUT_DIRECTORY_NAME, file_name)
+file_name = config.TEST_FILE_NAME
+file_path = os.path.join(config.OUTPUT_DIRECTORY_NAME, config.TEST_FILE_NAME)
 f = open(file_path, 'w')
 f.writelines(edi_file)
 f.close()

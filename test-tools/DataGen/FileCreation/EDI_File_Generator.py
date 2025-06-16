e4f7d0ba0153de0d8537606e007382979867d6e0
ryanl <ryan.y.lee0509@gmail.com>
2025-06-16 16:22:36 -0400

    adjustable error rates

--- test-tools/DataGen/FileCreation/EDI_File_Generator.py
+++ test-tools/DataGen/FileCreation/EDI_File_Generator.py
@@ -3,9 +3,11 @@ import uuid
 import FileCreation.EDISegments as Seg
 import config
 from DataLayer.Datatypes import Sponsor
+from FileCreation.ErrorController import ErrorCheck
 from config import get_logger
 
 logger = get_logger(__name__)
+
 
 class EDI834Generator:
     def __init__(self, sender=config.SENDER_ID, receiver=config.RECEIVER_ID, relationship_map=config.RELATIONSHIP_MAP):
@@ -14,7 +16,7 @@ class EDI834Generator:
         self.relationship_map = relationship_map
         self.transaction_control_number = 0
 
-    def create_member(self, member):
+    def create_member(self, member, error_ctrl):
         self.transaction_control_number += 1
 
         if isinstance(member, Sponsor):
@@ -28,21 +30,22 @@ class EDI834Generator:
 
         segments = [Seg.ST(self.transaction_control_number).to_edi(),
                     Seg.BGN(uuid.uuid4().hex.upper()).to_edi(),
-                    Seg.N1("P5", member.insurance_company, member.insurance_FID).to_edi(),
-                    Seg.N1("IN", member.insurance_company, member.insurance_FID).to_edi(),
+                    Seg.N1("P5", member.insurance_company, member.insurance_FID, error_ctrl).to_edi(),
+                    Seg.N1("IN", member.insurance_company, member.insurance_FID, error_ctrl).to_edi(),
                     Seg.INS(relationship_code).to_edi(),
-                    Seg.REF("0F", sponsor_id).to_edi(),
-                    Seg.REF("6O", beneficiary_id).to_edi(),
-                    Seg.NM1(member.last_name, member.first_name, member.middle_name, member.ssn).to_edi(),
-                    Seg.PER(member.phone).to_edi(),
-                    Seg.N3(member.address.building_number, member.address.street, member.address.apartment).to_edi(),
-                    Seg.N4(member.address.city, member.address.state, member.address.zipcode).to_edi()
+                    Seg.REF("0F", sponsor_id, error_ctrl).to_edi(),
+                    Seg.REF("6O", beneficiary_id, error_ctrl).to_edi(),
+                    Seg.NM1(member.last_name, member.first_name, member.middle_name, member.ssn, error_ctrl).to_edi(),
+                    Seg.PER(member.phone, error_ctrl).to_edi(),
+                    Seg.N3(member.address.building_number, member.address.street, member.address.apartment,
+                           error_ctrl).to_edi(),
+                    Seg.N4(member.address.city, member.address.state, member.address.zipcode, error_ctrl).to_edi()
                     ]
 
         for code, value in member.deductibles.items():
-            segments.append(Seg.AMT(code, str(value)).to_edi())
+            segments.append(Seg.AMT(code, str(value), error_ctrl).to_edi())
         for code, value in member.visit_counts.items():
-            segments.append(Seg.AMT(code, str(value)).to_edi())
+            segments.append(Seg.AMT(code, str(value), error_ctrl).to_edi())
 
         segments += [Seg.HD().to_edi(),
                      Seg.DTP().to_edi(),
@@ -52,10 +55,12 @@ class EDI834Generator:
         return segments
 
     def create_transaction(self, sponsor):
+        error_ctrl = ErrorCheck(error_rate=config.TOTAL_ERROR_RATE, error_type=config.ERROR_TYPES)
+
         segments = []
-        segments.extend(self.create_member(sponsor))
+        segments.extend(self.create_member(sponsor, error_ctrl))
         for beneficiary in sponsor.beneficiaries:
-            segments.extend(self.create_member(beneficiary))
+            segments.extend(self.create_member(beneficiary, error_ctrl))
         return segments
 
     def combine_segments(self, sponsors):

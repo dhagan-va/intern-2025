8dd597352c134b7cb53388b45b60b05744eeadbd
ryanl <ryan.y.lee0509@gmail.com>
2025-06-18 10:10:23 -0400

    unittest for error rate checking

--- test-tools/DataGen/UnitTests/test.py
+++ test-tools/DataGen/UnitTests/test.py
@@ -21,9 +21,28 @@ class TestDuplicateSSNs(unittest.TestCase):
         self.assertEqual(len(ssns), len(unique_ssns), "Duplicate SSNs found")
 
 
-# class TestErrorRate(unittest.Testcase):
-#     def test_error_rates(self):
-#         self.assertTrue()
+class TestErrorRate(unittest.TestCase):
+    def test_error_rates(self):
+        messages = 100000
+        expected_error_rate = config.TOTAL_ERROR_RATE
+        error_check = ErrorCheck(max_messages=messages)
+
+        actual_inserts = 0
+        for _ in range(messages):
+            if error_check.should_insert():
+                error_check.insert("test", "missing")
+                actual_inserts += 1
+
+        # check if error_count and inserts are same
+        self.assertEqual(error_check.error_count, actual_inserts)
+
+        expected_errors = (expected_error_rate * messages)
+        # 10% margin
+        margin = int(0.1 * expected_errors)
+
+        # Checks if the error count is within the margin
+        self.assertTrue(abs(actual_inserts - expected_errors) <= margin,
+                        f"Actual errors: {actual_inserts}, Expected: {expected_errors} {u"\u00B1"} {margin}")
 
 
 # Make message, test if contents are valid, make sure it's not the same every time

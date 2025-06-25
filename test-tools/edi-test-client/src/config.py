0d6cc3fc0c5f998b82f88f8e44bc561474fd1941
chrissu777 <christophersu777@gmail.com>
2025-06-25 12:08:54 -0400

    adding types to client

--- test-tools/edi-test-client/src/config.py
+++ test-tools/edi-test-client/src/config.py
@@ -2,22 +2,28 @@ from dataclasses import dataclass
 import tomllib
 from pathlib import Path
 
-DEFAULT_CONF = Path(__file__).parent / 'conf' / 'default.toml'
+DEFAULT_CONF = Path(__file__).parent / "conf" / "default.toml"
+
 
 @dataclass
 class Setting:
     """
     Class representing configuration settings for test client.
     """
+
     endpoint: str
     rps: float
     threads: int
-    def __init__(self, endpoint : str, rps : float, threads : int):
+    transaction: int
+
+    def __init__(self, endpoint: str, rps: float, threads: int, transaction: int):
         self.endpoint = endpoint
         self.rps = rps
         self.threads = threads
+        self.transaction = transaction
+
 
 def load_settings():
-    with open(DEFAULT_CONF, 'rb') as f:
+    with open(DEFAULT_CONF, "rb") as f:
         data = tomllib.load(f)
-        return Setting(data['endpoint'], data['rps'], data['threads'])
+        return Setting(data["endpoint"], data["rps"], data["threads"], data["transaction"])

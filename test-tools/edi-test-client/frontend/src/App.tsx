0f0e144a72c45ee2d3b2637c64e0cd9e2f2243b4
chrissu777 <christophersu777@gmail.com>
2025-06-30 20:00:07 -0400

    connecting basic button to api

--- test-tools/edi-test-client/frontend/src/App.tsx
+++ test-tools/edi-test-client/frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import React, {useState} from 'react'
 
+const startClick = () => {
+  
+}
 function App() {
   return (
     <>
-
+    <button onClick={}>START</button>
     </>
   )
 }

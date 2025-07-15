109e38b837ee84c1a7cbb6f598827b60532abca1
ryanl <ryan.y.lee0509@gmail.com>
2025-07-15 10:26:19 -0400

    cleaned up interface functions

--- test-tools/DataGen/src/DataLayer/Interfaces.py
+++ test-tools/DataGen/src/DataLayer/Interfaces.py
@@ -23,50 +23,21 @@ class DataAccess(ABC):
     @abstractmethod
     def get_sponsor_by_id(self, sponsor_id): pass
 
-    @abstractmethod
-    def get_sponsor_field(self, sponsor_id, field): pass
-
-    @abstractmethod
-    def update_sponsor_field(self, sponsor_id, field, value): pass
-
     # Beneficiary methods
     @abstractmethod
     def get_beneficiary(self, sponsor_id, beneficiary_id): pass
 
     @abstractmethod
-    def get_beneficiary_field(self, sponsor_id, beneficiary_id, field): pass
+    def save_claim_transaction(self, claim, commit): pass
 
     @abstractmethod
-    def update_beneficiary_field(self, sponsor_id, beneficiary_id, field, value): pass
+    def save_many_claims(self, claims): pass
 
     @abstractmethod
-    def get_bene_deductible(self, sponsor_id, beneficiary_id, code): pass
+    def get_claim_transactions(self, status, date): pass
 
     @abstractmethod
-    def get_bene_visit(self, sponsor_id, beneficiary_id, code): pass
+    def update_claims_status(self, claim_ids, new_status): pass
 
     @abstractmethod
-    def update_bene_deductible(self, sponsor_id, beneficiary_id, code, value): pass
-
-    @abstractmethod
-    def update_bene_visit(self, sponsor_id, beneficiary_id, code, value): pass
-
-    @abstractmethod
-    def save_claim_transaction(self, claim, commit):
-        pass
-
-    @abstractmethod
-    def save_many_claims(self, claims):
-        pass
-
-    @abstractmethod
-    def get_claim_transactions(self, status, date):
-        pass
-
-    @abstractmethod
-    def update_claims_status(self, claim_ids, new_status):
-        pass
-
-    @abstractmethod
-    def total_claim_transactions(self):
-        pass
+    def total_claim_transactions(self): pass
